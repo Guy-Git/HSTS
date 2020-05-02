a6cced6e3629bb5d760c0793f159c7c97b950100
linoymoshe <moshelinoy@gmail.com>
2020-05-02 17:02:09 +0300

    hi hi hi

--- src/main/java/il/ac/haifa/cs/sweng/OCSFSimpleChat/ChatClientCLI.java
+++ src/main/java/il/ac/haifa/cs/sweng/OCSFSimpleChat/ChatClientCLI.java
@@ -16,7 +16,7 @@ public class ChatClientCLI {
 		this.client = client;
 		this.isRunning = false;
 	}
-/*hi hi*/
+/*hi hi hi*/
 	public void loop() throws IOException {
 		loopThread = new Thread(new Runnable() {
 
