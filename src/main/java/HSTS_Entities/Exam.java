bceaf067924d9d1ae4ab224e7d9d82167952345f
Opal <Opal@DESKTOP-J4COD1K>
2020-06-01 17:39:48 +0300

    broken exam

--- src/main/java/HSTS_Entities/Exam.java
+++ src/main/java/HSTS_Entities/Exam.java
@@ -2,27 +2,57 @@ package HSTS_Entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Table(name = "exams")
 public class Exam implements Serializable
-{
+{	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	public Exam(long id, ArrayList<Question> questions, String notes) {
+	
+	@ManyToMany ( 
+		cascade = {javax.persistence.CascadeType.PERSIST, javax.persistence.CascadeType.MERGE},
+		targetEntity = Question.class)
+	@JoinTable(name="exams_questions", joinColumns = @JoinColumn(name="exam_id"),inverseJoinColumns = @JoinColumn(name="question_id"))
+	private List<Question> questions;
+	
+	private String notes;
+	private String teacherName;
+	private int time;
+	private ArrayList<Integer> questionGrade;
+	
+	public Exam(ArrayList<Question> questions, String notes, String teacherName, int time,
+			ArrayList<Integer> questionGrade) {
 		super();
-		this.id = id;
-		this.questions = questions;
+		this.questions = new ArrayList<Question>();
 		this.notes = notes;
+		this.teacherName = teacherName;
+		this.time = time;
+		this.questionGrade = questionGrade;
 	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public ArrayList<Question> getQuestions() {
+	public List<Question> getQuestions() {
 		return questions;
 	}
-	public void setQuestions(ArrayList<Question> questions) {
+	
+	public void setQuestions(List<Question> questions) {
 		this.questions = questions;
+		for(Question question : questions) {
+			question.getExams().add(this);
+		}
 	}
 	public String getNotes() {
 		return notes;
@@ -30,7 +60,24 @@ public class Exam implements Serializable
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
-	private ArrayList<Question> questions;
-	private String notes;
+	public String getTeacherName() {
+		return teacherName;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public ArrayList<Integer> getQuestionGrade() {
+		return questionGrade;
+	}
+	public void setQuestionGrade(ArrayList<Integer> questionGrade) {
+		this.questionGrade = questionGrade;
+	}
+	
 	
 }
